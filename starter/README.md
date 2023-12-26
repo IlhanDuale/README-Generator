b7779ad4ade5b9dff6c8af3a1538d6a3eddc8f25
Ilhan <dualeilhan@gmail.com>
2023-12-26 19:23:37 +0000

    completed task and created readme file

---
+++ starter/README.md
@@ -0,0 +1,30 @@
+# README Generator
+
+## Description
+This project is a command-line tool that dynamically creates a professional README.md file based on user input, facilitating quick and standardized documentation for new projects.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install the project, run the command npm install
+
+## Usage
+To use the project run the following command line node index.js
+
+## License
+This project is licensed under the MIT License.
+
+## Contributing
+To contribute to the project follow github guidlines
+
+## Tests
+To run tests for the project run the following command: npm test
+
+## Questions
+If you have any questions, you can reach me at [GitHub: IlhanDuale](https://github.com/IlhanDuale) or via email at dualeilhan@gmail.com.
---
+++ starter/index.cjs
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require('path');
+const { prompt } = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [{
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'Other'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you run tests for your project?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+}
+
+// function to initialize program
+function init() {
+    prompt(questions)
+        .then((answers) => {
+            const readmeContent = generateMarkdown(answers);
+            const outputPath = path.join(process.cwd(), 'README.md');
+            writeToFile(outputPath, readmeContent);
+            console.log(`README generated at ${outputPath}`);
+        })
+        .catch((error) => console.error('Error:', error));
+}
+
+
+// function call to initialize program
+init();
